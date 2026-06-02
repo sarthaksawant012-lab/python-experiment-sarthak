afb1fddae5d832723692b320f3bb25cfc08afd78
sarthaksawant012-lab <sarthaksawant012@gmail.com>
2026-06-02 21:27:51 +0530

    Add files via upload

---
+++ exp no 9 sarthak sawant.py
@@ -0,0 +1,21 @@
+#Dictionary
+a={47:"sudin", 54:"sarthak", 50:"raunak", 4:"pradnyesh", "CT":"D D Nale"}
+print(a)
+
+#Checking Datatype
+print(type(a))
+
+#checking legth of Dict
+print(len(a))
+
+#Accessing keys
+print(a.keys())
+
+#Accessing values
+print(a.values())
+
+#Accessing the value from key
+print(a['CT'])
+      
+#converting dict in to List or tuple
+print(tuple(a))
